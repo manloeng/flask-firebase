072cf5fac7b88fbd07883259cd88992cd4ab7192
Andrew <manloengchung@yahoo.co.uk>
2019-08-16 19:51:38 +0100

    Refactore Code

--- route.py
+++ route.py
@@ -18,20 +18,20 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
 
-@app.route('/', methods=['GET', 'POST'])
-def basic():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     unsuccessful = 'Please check your credentials'
     successful = 'Login successful'
     if request.method == 'POST':
-        email = request.form['name']
-        password = request.form['pass']
+        email = request.form['email']
+        password = request.form['password']
         try:
             auth.sign_in_with_email_and_password(email, password)
-            return render_template('new.html', s=successful)
+            return render_template('login.html', s=successful)
         except:
-            return render_template('new.html', us=unsuccessful)
+            return render_template('login.html', us=unsuccessful)
 
-    return render_template('new.html')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
